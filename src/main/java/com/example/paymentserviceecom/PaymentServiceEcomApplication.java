bb253514506e67e739616641df5d07b19197d35f
Prochnost55 <aks3555@gmail.com>
2024-03-18 19:33:25 +0530

    init comit

---
+++ src/main/java/com/example/paymentserviceecom/PaymentServiceEcomApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentserviceecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paymentserviceecom/PaymentServiceEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentserviceecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
